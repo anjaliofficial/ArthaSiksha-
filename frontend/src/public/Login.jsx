1fd845ef2f7e40b404e54e4d6126c3eb1b62c919
apala-1 <apalalamichhane13579@gmail.com>
2025-08-22 15:22:25 +0545

    Login Page Frontend Completed

--- frontend/src/public/Login.jsx
+++ frontend/src/public/Login.jsx
@@ -1,7 +1,24 @@
+import logoWhite from '../assets/logoWhite.png';
+import { IoArrowBackOutline } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import './Login.css';
+
 const Login = () => {
     return(
-        <div>
-
+        <div className="whole-page">
+            <div className="login-container">
+                <a className='backBtn' href="#"><IoArrowBackOutline /></a>
+                <img className='company-logo' src={logoWhite} alt="logo" />
+                <input className='inputEmail' type="email" placeholder='email' />
+                <input className='inputPassword' type="password" placeholder='********'/>
+                <button className='loginBtn'>Log In</button>
+                <a className='forgotPassBtn' href="#">Forgot Password?</a>
+                <div className='social-icons'>
+                    <a href="#" className='google-icon'><FcGoogle /></a>
+                    <a href="#" className='facebook-icon'><FaFacebook /></a>
+                </div>
+            </div>
         </div>
     );
 };
